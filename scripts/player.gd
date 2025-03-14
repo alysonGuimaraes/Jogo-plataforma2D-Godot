94869f1ca57b905b0a9b4ac049b00788b9703af2
Alyson Guimarães da Silva <116230343+alysonGuimaraes@users.noreply.github.com>
2025-03-13 21:39:31 -0300

    Aula 13_03
    Adição de novas cenas, background e adição de tiles sets salvos. Adição de tile set animado para água. Pequeno ajuste na câmera.

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,8 +5,8 @@ extends CharacterBody2D
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
-
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
