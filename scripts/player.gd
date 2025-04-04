1fe50c999b22b0c49eaec77e90f1c60d9e256d82
Alyson Guimarães da Silva <116230343+alysonGuimaraes@users.noreply.github.com>
2025-04-03 21:55:12 -0300

    Modificações para aula dia 03-04
    Modificações para entrega da aula.

--- scripts/player.gd
+++ scripts/player.gd
@@ -88,14 +88,21 @@ func input_attack(direction):
 	else:
 		state = GROUND
 	
-func _on_area_2d_area_entered(area: Area2D) -> void:
+func _on_area_2d_area_entered(area: Area2D) -> void:	
 	if area.is_in_group("DeathZones"):
-		get_tree().reload_current_scene()
+		call_deferred("reload_scene")
 	elif area.is_in_group("EndZones"):
-		get_tree().change_scene_to_file("res://scenes/forest.tscn")
+		var next_scene = area.next_level
+		call_deferred("load_scene", next_scene)
 	elif area.is_in_group("change_level_zone"):
 		player_inside = true
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if area.is_in_group("change_level_zone"):
 		player_inside = false
+
+func reload_scene():
+	get_tree().reload_current_scene()
+	
+func load_scene(scene: String):
+	get_tree().change_scene_to_file("res://scenes/" + scene + ".tscn")
